d310f67c6fbfed75545527e7060cdb361e034b5c
Sholawasiu <sholawasiu92@gmail.com>
2022-09-08 08:40:03 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
 * Description: using the main function
 * this programe print with proper grammar, but the outcome is a piece of art,
 *
-* return: 0, otherwise non zero
+* return: 0 at the end of execution, otherwise non zero
 *
 */
 
