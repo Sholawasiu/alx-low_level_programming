1e1ef5c16fdbcdc3a897633bb748838d39f3840d
Sholawasiu <sholawasiu92@gmail.com>
2022-09-08 07:30:26 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
 /**
-
+ *
  * main - prints the size of various types
-
+ *
  * Return: 0 
-
+ *
  */
 
 int main(void)
-  
 {
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
+  puts("\"Programming is like building a multilingual puzzle"); 
   return (0);
-  
 }
