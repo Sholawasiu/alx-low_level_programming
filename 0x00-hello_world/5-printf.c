10bb9e0c8252485ad4982d1dea9da5f8673a7f72
Sholawasiu <sholawasiu92@gmail.com>
2022-09-08 07:44:50 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the printf function
+ *
+ *return: 0
+ *
+ */
+
+int main(void)
+
+{
+
+printf("\"with proper grammar, but the outcome is a piece of art,");
+
+return(0);
+
+}
