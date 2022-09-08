b01e4d91aa915d195c51501b443dd0529a87c374
Sholawasiu <sholawasiu92@gmail.com>
2022-09-08 13:47:30 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,6 @@
 /**
 * main - print the string in the printf function
 * Description: the programme print with proper grammar, but the outcome
-* return: 0 at the end of execution, otherwise return non zero
 */
 
 int main(void)
