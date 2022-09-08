3568f79dca67adad2931e10104130a443f943f7f
Sholawasiu <sholawasiu92@gmail.com>
2022-09-08 07:24:49 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
+
+
 /**
 
- * main - print the string in the put function
+ * main - prints the size of various types
 
- * this program prints "Programming is like building a multilingual puzzle
-
- * Return: 0
+ * Return: 0 
 
  */
 
 int main(void)
-
+  
 {
-
+  
   puts("\"Programming is like building a multilingual puzzle");
-
+  
   return (0);
-
+  
 }
