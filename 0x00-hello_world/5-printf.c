de105a2073b6048c3c94886adecbead6a1532e25
Sholawasiu <sholawasiu92@gmail.com>
2022-09-08 13:06:09 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
 * main - print the string in the printf function
 *
 * Description: using the main function
-* this programe print with proper grammar, but the outcome is a piece of art,
+* this programme prints with proper grammar, but the outcome is a piece of art,
 *
-* return: 0 at the end of execution, otherwise non zero
+* return: 0 at the end of execution, otherwise return non zero
 *
 */
 
