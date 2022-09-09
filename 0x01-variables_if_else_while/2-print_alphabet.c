bf9c6702b807cb9f677ae0cf8afd044abe4daa4a
Sholawasiu <sholawasiu92@gmail.com>
2022-09-09 10:32:39 -0700

    2.I sometimes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#inclusde <stdio.h>
+
+
+int main(void)
+
+{
+
+int ch;
+  
+for (ch = 97, ch <= 122, ch++)
+
+{
+putchar(ch);
+}
+
+for (ch = 65, ch <= 90, ch++)
+
+{
+putchar(ch);
+}
+
+putchar(10);
+
+return(0);
+
+}
+
+	
