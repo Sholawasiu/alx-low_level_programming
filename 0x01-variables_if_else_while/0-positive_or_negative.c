2c9b2e929392f3455565ee947b103c01fd52a045
Sholawasiu <sholawasiu92@gmail.com>
2022-09-09 09:40:55 -0700

    0.Positive anything

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /*
 * main: - assign a random number to the variable n each time it is executed
 *
-* and print the last digit of the number stored in the variable n
+* description - and print the last digit of the number stored in the variable n
 *
 * return: Always 0
 *
