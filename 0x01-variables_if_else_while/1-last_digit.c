bcb1d73c8a42d6c711f95a3ce7e1f717286c1bfd
Sholawasiu <sholawasiu92@gmail.com>
2022-09-09 10:12:51 -0700

    1.The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,6 +4,7 @@
 
 /*
 *main - assign a random number to the variable n each time it is executed
+*
 *Description: print the last digit of the assigned random number
 *
 *and print the last digit of the number stored in the variable n
