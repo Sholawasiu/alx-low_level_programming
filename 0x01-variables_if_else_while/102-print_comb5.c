d3763d6ba9d5367946e705f1090e118fd6b92454
Sholawasiu <sholawasiu92@gmail.com>
2022-09-10 09:09:03 -0700

    Software

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description: print all possible combination of two two-digit numbers
+*Return: Always (0)
+*
+*/
+
+int main(void)
+{
+int k,l;
+for (k = 0; k < 100; k++)
+for (l = 0; l < 100; l++)
+if (k < l)
+putchar((k / 10) + 48);
+putchar((k % 10) + 48);
+putchar(' ');
+putchar((l / 10) + 48);
+putchar((l % 10) + 48);
+if (k != 98 || l != 99)
+putchar(',');
+putchar(' ');
+putchar('\n');
+return (0);
+ }
