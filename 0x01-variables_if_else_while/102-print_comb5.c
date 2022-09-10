4a427849f00e4ba084ac104558756173c5cbe0fd
Sholawasiu <sholawasiu92@gmail.com>
2022-09-10 09:14:41 -0700

    Software

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,18 +9,32 @@
 
 int main(void)
 {
-int k,l;
+int k, l;
 for (k = 0; k < 100; k++)
 for (l = 0; l < 100; l++)
 if (k < l)
+{
 putchar((k / 10) + 48);
+}
+{
 putchar((k % 10) + 48);
+}
+{
 putchar(' ');
+}
+{
 putchar((l / 10) + 48);
+}
+{
 putchar((l % 10) + 48);
+}
 if (k != 98 || l != 99)
+{
 putchar(',');
+}
+{
 putchar(' ');
+}
 putchar('\n');
 return (0);
- }
+}
