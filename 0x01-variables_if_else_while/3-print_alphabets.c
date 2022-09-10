771f33263daa6e52bb3e656b88e9f54044258159
Sholawasiu <sholawasiu92@gmail.com>
2022-09-10 03:24:01 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,7 +7,7 @@ int main(void)
 {
 int ch;
 
-for (ch=97; ch <= 122; ch++)
+for (ch = 97; ch <= 122; ch++)
 {
 putchar(ch);
 }
@@ -15,6 +15,6 @@ for (ch = 65; ch <= 90; ch++)
 {
 putchar(ch);
 }
-putchar(10); 
+putchar(10);
 return (0);
 }
