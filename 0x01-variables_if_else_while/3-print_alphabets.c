4e7c4278ad68a283d3aaef25ddba40a1c0f8aa15
Sholawasiu <sholawasiu92@gmail.com>
2022-09-10 03:14:46 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 
-int main (void)
+int main(void)
 
 {
 int ch;
 
- for (ch=97; ch <= 122; ch++)
+for (ch=97; ch <= 122; ch++)
 {
 putchar(ch);
 }
- for (ch = 65; ch <= 90; ch++)
+for (ch = 65; ch <= 90; ch++)
 {
 putchar(ch);
 }
