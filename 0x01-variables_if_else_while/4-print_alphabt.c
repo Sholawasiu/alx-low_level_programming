6737fb7d73782b3091bc54bf8ebb17351c015566
Sholawasiu <sholawasiu92@gmail.com>
2022-09-10 03:40:10 -0700

    When i was

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main: assign a random number to variable n each time it is executed
+*
+*print th last digit of the number stored in the variable n
+*
+*/
+
+
+int main(void)
+
+{
+int ch;
+
+
+for (ch = 'a'; ch <= 'z'; ch++)
+
+if (ch != q; && ch != e; ch++)
+
+{ 
+putchar(ch);
+}
+putchar(10); /*ascii code for new line*/
+return (0);
+}
