d9a11f1cb66fcecc3e990fb303d2ebcc3c980cc9
Sholawasiu <sholawasiu92@gmail.com>
2022-09-10 05:36:30 -0700

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <srdio.h>
+#include <stdlib.h>
+
+/**
+*main - Entry point
+*Description: print all possible combination of singlr-digit number
+*Return: Always (0)
+*
+*/
+
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 57, ch++)
+{
+putchar(ch);
+}
+if (ch != 57)
+{
+putchar(44);
+putchar(32);
+}
+putchar(10);
+return (0);
+}
