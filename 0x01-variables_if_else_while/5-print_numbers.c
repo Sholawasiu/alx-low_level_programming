879feaee7d6232a41244d0b8a1bfb793a5878a8b
Sholawasiu <sholawasiu92@gmail.com>
2022-09-10 04:42:46 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,16 +4,16 @@
 /**
 *main - Entry point
 *Description: print all single digit number of base 10 starting from 0
-*
+*Return: Always (0)
 */
 
 int main(void)
 
 {
-  int n;
+int n;
 
 for (n = 0; n < 10; n++)
-printf("%d",n);
+printf("%d", n);
 printf("/n");
 return (0);
 }
