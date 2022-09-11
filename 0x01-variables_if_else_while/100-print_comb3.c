8eb1a1e08a5e88548ea4afd86c1fedb725b57e72
Sholawasiu <sholawasiu92@gmail.com>
2022-09-11 06:37:13 -0700

    Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,11 @@
 */
 int main(void)
 {
-int c = 0;
-while (c <= 99)
+int c = 1;
+while (c <= 89)
 {
-putchar(c / 10 + '0');
-putchar(c % 10 + '0');
+putchar(c / 10 + '1');
+putchar(c % 10 + '1');
 if (c != 99)
 {
 putchar(',');
