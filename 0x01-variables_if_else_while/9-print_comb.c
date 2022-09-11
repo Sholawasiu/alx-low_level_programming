c563f629829c90f8ffee4969b46ab58face9c555
Sholawasiu <sholawasiu92@gmail.com>
2022-09-11 05:37:03 -0700

    Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,10 @@ int main(void)
 {
 int n;
 for (n = 48; n <= 57; n++)
-
-{putchar(n);
+{putchar(n);}
 if (n != 57)
-putchar(',');
-putchar(' ');
-putchar('\n');}
+{putchar(',');}
+{putchar(' ');}
+{putchar('\n');}
 return (0);
 }
