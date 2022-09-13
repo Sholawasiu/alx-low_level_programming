44476bd0cf88486448054b60e8128242fb6aae98
Sholawasiu <sholawasiu92@gmail.com>
2022-09-13 03:06:03 -0700

    __putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+*main - Entry level
+*Description: this program prints _putchar
+*Return: Always (0)
+*
+*/
+int main(void)
+{
+char n[]="_putchar";
+{
+printf("%s\n",n);
+}
+return (0);
+}
