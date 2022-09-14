f773e507e98aba0ec095b5fbef105116d689dc6a
Sholawasiu <sholawasiu92@gmail.com>
2022-09-14 03:38:45 -0700

    I sometimes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,12 @@
 *print_alphabet - function that print alphabet
 *it prints in lowercase
 *Return: no return
-*
 */
 
 void print_alphabet(void)
 {
 int alphabet;
-for (alphabet = 'a' ; alphabet <= 'z'; alphabet++);
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet;
 }
