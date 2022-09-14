2fb96456c64b45d0deb00234cf5151f4ddc3225f
Sholawasiu <sholawasiu92@gmail.com>
2022-09-14 03:33:04 -0700

    I sometimes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
+
 /**
 *print_alphabet - function that print alphabet
 *it prints in lowercase
 *Return: no return
 *
 */
+
 void print_alphabet(void)
 {
-char n;
+int n;
 for (n = 'a' ; n <= 'z'; n++);
-{
 _putchar(n);
-}
 _putchar(10);
 }
 
