c49320ec81bd1a417eb69c15c8542e2ac83d2263
Sholawasiu <sholawasiu92@gmail.com>
2022-09-14 02:34:08 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
 *main - Entry point
-*Description: checking for alphabetic character
-*Return: 0 or 1
-*
+*Description: checks for alphabetic character
+*Return:0 or 1
 */
-
 int _isalpha(int c)
 {
 return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
