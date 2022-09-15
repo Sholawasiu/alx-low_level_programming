43f5ea831f59c490edf693fe756b4f29bada5957
Sholawasiu <sholawasiu92@gmail.com>
2022-09-15 11:04:08 -0700

    Numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print numbers of 0 to 14 10x
+*/
+
+void more_numbers(void);
+{
+int x, y;
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y < 15; y++)
+{
+if (y >= 10)
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
+}
+_putchar('\n');
+}
+}
