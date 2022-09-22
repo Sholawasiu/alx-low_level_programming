2fc98a2942c09cc05ccf7d5e655aebc21681a456
Sholawasiu <sholawasiu92@gmail.com>
2022-09-22 02:47:54 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <string.h>
 
-
-
 /**
 * -strncat - function to append some char
 * @dest: first parameter1
@@ -13,6 +11,6 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-_strncat(dest, src, n);
+strncat(dest, src, n);
 return (dest);
 }
