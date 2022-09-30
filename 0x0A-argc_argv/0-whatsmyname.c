f86beb43d31bb6dffcf523f84deedd237323ea82
Sholawasiu <sholawasiu92@gmail.com>
2022-09-30 08:00:16 -0700

    It ain't

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - A program that prints its name follow by a new line
+*@argv: variable
+*Return: Always succes
+*/
+
+int main (int argc, char* argv[])
+{
+printf("%s", argv[0])
+return (argv);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+C argc and argv
+program that prints its name follow by a new line
