7c80cd5f78f8267c8809f039c9eff42f50204a95
Sholawasiu <sholawasiu92@gmail.com>
2022-09-30 09:20:15 -0700

    Silence

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - prints the number of argument pass into it
+*@argc: arg count
+*@argv: array of pointer to CLI arg
+*Return: success
+*/
+
+int main( int argc, char *argv[n])
+{
+printf("%d\n", argc[i]);
+}
