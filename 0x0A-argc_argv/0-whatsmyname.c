f4de833bd62783bf1c7167a17899aa720e825299
Sholawasiu <sholawasiu92@gmail.com>
2022-10-02 08:26:04 -0700

    It ain't

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,11 @@
 *main - A program that prints its name follow by a new line
 *@argv: variable1
 *@argc: variable2
-*Return: Always argv[0]
+*Return: Always (0)
 */
 
-int main(int argc, char *argv[0])
-{
-printf("%s\n", argv[0]);
-return (argv);
+int main(int argc, char *argv[]) 
+{  
+printf("%s\n", argv[argc - 1]);  
+return (0); 
 }
