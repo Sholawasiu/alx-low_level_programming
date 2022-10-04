5449ced69603616c2e811c0d67e29fbdf224d725
Sholawasiu <sholawasiu92@gmail.com>
2022-10-04 05:34:31 -0700

    Float

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,26 +1,23 @@
+#include "main.h"
 #include <stdlib.h>
-#include "holberton.h"
 
 /**
-* create_array - check the code for Holberton School students.
-* @size: argument for size of the array
-* @c: argument for char
-* Return: Always 0.
+* create_array - creates an array of chars.
+* @size: size of the array.
+* @c: storaged char
+* Return: pointer of an array of chars
 */
 
 char *create_array(unsigned int size, char c)
 {
-char *my_array;
-unsigned int i = 0;
-my_array = malloc(size * sizeof(c));
-while (i < size)
-{
-if (my_array == NULL)
-{
+char *cr;
+unsigned int i;
+if (size == 0)
 return (NULL);
-}
-my_array[i] = c;
-i++;
-}
-return (my_array)
+cr = malloc(sizeof(c) * size);
+if (cr == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+cr[i] = c;
+return (cr);
 }
