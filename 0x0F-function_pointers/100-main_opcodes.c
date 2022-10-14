e10d839c6e5bcbb2020da4917dc7e936c610c221
Sholawasiu <sholawasiu92@gmail.com>
2022-10-14 11:50:05 -0700

    Most

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,9 +3,9 @@
 
 
 /**
-* main - check the code for Holberton School students.
-* @argc: argument count.
-* @argv: argument vector.
+* main - check the code 
+* @argc: argument count
+* @argv: argument vector
 *
 * Return: Always 0.
 */
