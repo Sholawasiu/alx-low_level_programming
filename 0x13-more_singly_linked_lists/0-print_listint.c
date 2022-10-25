0d77b31b03b8394231bab2dd86769a8e19d48ba2
Sholawasiu <sholawasiu92@gmail.com>
2022-10-25 13:58:31 -0700

    Print lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,17 @@
 */
 
 size_t print_listint(const listint_t *h)
-{
-newnode = 0;
-while (h != NULL)
-{
-printf("%d", h->n);
-h=h->next
-newnode++;
-}
-return newnode;
+{  
+size_t newnode;  
+newnode = 0;  
+while (h != NULL)    
+{      
+if (h->str == NULL)	
+printf("[%d] %s\n", 0, "(nil)");      
+else	
+printf("[%d] %s\n", h->len, h->str);      
+h = h->next;      
+newnode++;      
+}  
+return (newnode); 
 }
