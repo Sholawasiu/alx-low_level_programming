9ff8df8c6aeef5b234403834e09a43777a690b77
Sholawasiu <sholawasiu92@gmail.com>
2024-01-22 06:57:42 -0800

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,38 +12,33 @@
 
 
 
-void calculateChange(int cents) {
-    if (cents < 0) {
-        printf("0\n");
-        return;
-    }
-
-    int coins[] = {25, 10, 5, 2, 1};
-    int numCoins = sizeof(coins) / sizeof(coins[0]);
-
-    int count = 0;
-    for (int i = 0; i < numCoins; ++i) {
-        while (cents >= coins[i]) {
-            cents -= coins[i];
-            count++;
-        }
-    }
-
-    printf("%d\n", count);
+void calculateChange(int cents) 
+{
+if (cents < 0)
+{
+printf("0\n");
+return;
 }
-
-int main(int argc, char *argv[]) {
-    // Check the number of arguments
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
-
-    // Convert the argument to an integer
-    int cents = atoi(argv[1]);
-
-    // Calculate and print the minimum number of coins
-    calculateChange(cents);
-
-    return 0;
+int coins[] = {25, 10, 5, 2, 1};
+int numCoins = sizeof(coins) / sizeof(coins[0]);
+int count = 0;
+for (int i = 0; i < numCoins; ++i)
+{
+while (cents >= coins[i])
+{
+cents -= coins[i];
+count++;
 }
+}
+printf("%d\n", count);
+}
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+printf("Error\n");
+return 1;
+}
+int cents = atoi(argv[1]);
+calculateChange(cents);
+return 0;
